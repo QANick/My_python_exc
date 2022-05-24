16f2c4209b0f6eb22ee03d4e57776cedeef8aa96
QANick <nboeshko@yadex.ru>
2022-05-24 18:10:38 +0300

    Added Exc 18.6.1

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = '5394373723:AAFN57uS6vJPi3l4lb9vKv-fL3YhDSNCY0s'
+
+
+keys = {
+    'юань': 'CNY',
+    'йена': 'JPY',
+    'доллар': 'USD',
+    'eвро': 'EUR',
+    'франк': 'CHF',
+    'лира': 'TRY',
+    'рубль': 'RUB',
+}
